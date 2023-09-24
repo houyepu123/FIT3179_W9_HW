abdae0c444d4ce185627a2410091de34a654498e
houyepu123 <126455847+houyepu123@users.noreply.github.com>
2023-09-24 20:27:44 +1000

    Add files via upload

---
+++ vega_lite.js
@@ -0,0 +1,5 @@
+var vg_1 = "vega_lite.json"; 
+
+vegaEmbed("#bar_chart", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
